4cb0957ccb6b8888e03599d29cd14b1df6a094cb
Diego Cardoso <diegocar448@hotmail.com>
2025-02-13 16:28:16 -0300

    estruturando model

--- app/src/main/java/com/diego/appdecafeteriaversao2/datasource/DataSource.kt
+++ app/src/main/java/com/diego/appdecafeteriaversao2/datasource/DataSource.kt
@@ -1,6 +1,45 @@
 package com.diego.appdecafeteriaversao2.datasource
 
+import com.diego.appdecafeteriaversao2.R
+import com.diego.appdecafeteriaversao2.model.Produto
 import javax.inject.Inject
 
 class DataSource @Inject constructor() {
+
+    fun getProdutos(
+        response: (MutableList<Produto>)
+    ){
+        val listaProdutos = mutableListOf(
+            Produto(
+                foto = R.drawable.cappuccino_tradicional,
+                nome = "Cappuccino tradicional",
+                preco = "15.00"
+            ),
+            Produto(
+                foto = R.drawable.cafe_expresso,
+                nome = "Café Expresso",
+                preco = "9.90"
+            ),
+            Produto(
+                foto = R.drawable.cappuccino_chocolate,
+                nome = "Cappuccino Gelado Sabor Chocolate",
+                preco = "25.00"
+            ),
+            Produto(
+                foto = R.drawable.cappuccino_pistache,
+                nome = "Cappuccino Pistache",
+                preco = "22.99"
+            ),
+            Produto(
+                foto = R.drawable.bolo_morango,
+                nome = "Bolo de Morango",
+                preco = "30.90"
+            ),
+            Produto(
+                foto = R.drawable.pudim,
+                nome = "Pudim de Leite",
+                preco = "16.00"
+            ),
+        )
+    }
 }
---
+++ app/src/main/java/com/diego/appdecafeteriaversao2/model/Produto.kt
@@ -0,0 +1,7 @@
+package com.diego.appdecafeteriaversao2.model
+
+data class Produto(
+    val foto: Int? = null,
+    val nome: String? = null,
+    val preco: String? = null,
+)

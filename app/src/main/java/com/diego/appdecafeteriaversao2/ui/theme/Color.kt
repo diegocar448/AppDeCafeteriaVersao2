562b1fc4d0d4907646c8a2ac33a237e9dc44f4c3
Diego Cardoso <diegocar448@hotmail.com>
2025-02-13 14:43:00 -0300

    cores definidas

--- app/src/main/java/com/diego/appdecafeteriaversao2/ui/theme/Color.kt
+++ app/src/main/java/com/diego/appdecafeteriaversao2/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.diego.appdecafeteriaversao2.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val WHITE = Color(0xFFFFFFFF)
+val BLACK80 = Color(0xFF121212)
+val BROWN400 = Color(0xFF8D6E63)
+val BROWN500 = Color(0xFF795548)
+val RED = Color(0xFFFF0000)
